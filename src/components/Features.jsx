e010e31ea328f518b21ab677c116dda83747890c
Lalord22 <lalo22296@hotmail.com>
2024-08-10 13:27:56 -0600

    Creates HowItWorks component
    Also includes the end to features and animation to features video

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -11,11 +11,26 @@ const Features = () => {
     const videoRef = useRef();
 
     useGSAP(() =>{
+        gsap.to('#exploreVideo', {
+            scrollTrigger: {
+                trigger:'#exploreVideo',
+                toggleActions: 'play pause reverse restart',
+                start: ' -10% bottom',
+            },
+            onComplete: () => {
+                videoRef.current.play();
+            }    
+        })
+
         animateWithGsap('#features_title', {y: 0, opacity: 1})
         animateWithGsap(
             '.g_grow',
             {scale:1, opacity: 1, ease: 'power1'},
             {scrub: 5.5}
+        )
+        animateWithGsap(
+            '.g_text',
+            {y: 0, opacity: 1, ease: 'power2.inOut', duration:1},
         )
     },[]);
 
@@ -44,12 +59,30 @@ const Features = () => {
                             <div className='overflow-hidden flex-1 h-[50vh]'>
                                 <img src={explore1Img} alt="titanium" className='feature-video g_grow' />
                             </div>
-                        </div>
-                    </div>
-                    <div className='flex flex-col w-full relative'>
-                        <div className='feature-video-container'>
                             <div className='overflow-hidden flex-1 h-[50vh]'>
                                 <img src={explore2Img} alt="titanium2" className='feature-video g_grow' />
+                            </div>
+                        </div>
+                        <div className='feature-text-container'>
+                            <div className='flex-1 flex-center'>
+                                <p className='feature-text g_text'>
+                                    iPhone 15 is {''}
+                                    <span className='text-white'>
+                                        the first iPhone to feature an aerospace-grade titanium design
+                                    </span>,
+                                    using the same alloy that spacecrafts are made of. It’s incredibly strong, yet deceptively light.
+                                </p>
+                            </div>
+
+                            <div className='flex-1 flex-center'>
+                                <p className='feature-text g_text'>
+                                    Titanium has one of the best strengh-to-weight ratios of any metal, 
+                                    <span className='text-white'>
+                                        making it the perfect material for iPhone 15
+                                    </span>,
+                                    . It’s also highly resistant to corrosion, so it will maintain its beauty for years to come.
+                                </p>
+                                
                             </div>
                         </div>
                     </div>
